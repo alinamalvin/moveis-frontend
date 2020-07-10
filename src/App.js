18460601c36b2b0624608a6375be8f0be3de2efc
github username <alinakornilova78@gmail.com>
2020-07-10 11:40:11 -0400

    install and import packages into index.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+
 class App extends React.Component {
+  
   render() {
     return (
        <div className="App">
+         App
        </div>
     );
   }
