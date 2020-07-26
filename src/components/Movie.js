486b704768852f401f1ef7ccfbff5d8a98e5319d
github username <alinakornilova78@gmail.com>
2020-07-26 17:03:12 -0400

    refactor Movie component to display all elements inline

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,11 +6,17 @@ import DeleteButton from './DeleteButton'
 export const Movie = (props) => {
     return (
         <div>
-            <LikeButton />
-            <ul key={props.movie.id} >
-                  {props.movie.name} ({props.movie.kind}) 
-            </ul>
+            <div style={{display: 'inline-block'}}><LikeButton /></div>
+            <div style={{display: 'inline-block'}}>
+            <h5>
+                <ul key={props.movie.id} >
+                  {props.movie.name} (genre: {props.movie.kind})
+                </ul>
+            </h5>
+            </div>
+            <div style={{display: 'inline-block'}}>
             <DeleteButton movie={props.movie}/>
+            </div>
         </div>
     )
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,20 +7,6 @@ const  Movies = (props) => {
     return (
         <div>
             {props.movies && props.movies.map(movie => <Movie movie={movie}/>)}
-              {/* <h5>
-                  <div style={{display: 'inline-block', flexDirection: 'column'}}><LikeButton /></div>
-              <div style={{display: 'inline-block', flexDirection: 'column'}}>
-              <ul key={movie.id} >
-                  {movie.name} ({movie.kind}) 
-                  <button type="button" 
-                          class="btn btn-outline-danger"
-                          onClick={() => handleDelete(movie)}>
-                              Delete
-                   </button>
-                </ul>
-               </div>
-               </h5>            
-               */}
             Total: {props.movies && props.movies.length}
         </div>
     )
