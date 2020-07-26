a27fdec1016ac80a248c928d3f0d4090c80339b0
github username <alinakornilova78@gmail.com>
2020-07-26 16:14:25 -0400

    align LikeButton with Movie name in Movies component

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {deleteMovie} from '../actions/deleteMovie'
 import LikeButton from './LikeButton'
 
+
 const  Movies = (props) => {
 
     const handleDelete = (movie) => {
@@ -12,7 +13,19 @@ const  Movies = (props) => {
     return (
         <div>
             {props.movies && props.movies.map(movie =>
-              <h5><LikeButton /><ul key={movie.id}>{movie.name} ({movie.kind}) <button type="button" class="btn btn-outline-danger" onClick={() => handleDelete(movie)}>Delete</button></ul></h5>            
+              <h5>
+                  <div style={{display: 'inline-block', flexDirection: 'column'}}><LikeButton /></div>
+              <div style={{display: 'inline-block', flexDirection: 'column'}}>
+              <ul key={movie.id} >
+                  {movie.name} ({movie.kind}) 
+                  <button type="button" 
+                          class="btn btn-outline-danger"
+                          onClick={() => handleDelete(movie)}>
+                              Delete
+                   </button>
+                </ul>
+               </div>
+               </h5>            
             )}
             Total: {props.movies && props.movies.length}
         </div>
