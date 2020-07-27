871a442b077734cd7530b6597d665f75f452041b
github username <alinakornilova78@gmail.com>
2020-07-26 20:04:28 -0400

    edit LikeButton to toggle between Like and not-Liked state

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -5,7 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export default class LikeButton extends Component {
 
 state = {
-    count: 0,
+    count: 'Like',
     updated: false
 }
 
@@ -23,30 +23,25 @@ handleChange = () => {
     if(!this.state.updated) {
         this.setState((prevState) => {
             return {
-                count: prevState.count + 1,
+                count: '❤️',
                 updated: true
             };
         });
+        toast.success('🦄 Liked!', this.toastStyle);
     } else {
         this.setState((prevState) => {
             return {
-                count: prevState.count - 1,
+                count: 'Like',
                 updated: false
             };
         });
     }
 }
-//   toast.success('🦄 Liked!', this.toastStyle);
-//   let newLike = this.state.count +1
-//   this.setState({
-//       count: newLike
-//   })
-
 
     render() {
         return (
             <div>
-               <button onClick={this.handleChange} type="button" class="btn btn-outline-secondary">❤️ Likes: {this.state.count}</button>
+               <button onClick={this.handleChange} type="button" class="btn btn-outline-secondary">{this.state.count} </button>
                <ToastContainer/>
             </div>
         )
